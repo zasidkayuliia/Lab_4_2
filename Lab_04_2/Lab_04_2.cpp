f61f18d3e2ba08bee49f76b5a144a3d68cee1d54
zasidkayuliia <yuliia.zasidka.ri.2024@lpnu.ua>
2024-09-25 03:57:45 +0300

    first commit

---
+++ Lab_04_2/Lab_04_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(7) << "y" << "    |" << endl;
+	cout << "----------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 4.95 * x * x;
+		if (x < -3.5)
+			B = 4 + 1 / (x * x);
+		else
+			if (x >= 1)
+				B = sin(3 * x) - cos(x);
+			else
+				B = tan((3.5 + x) / 5);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "----------------------" << endl;
+	return 0;
+}
